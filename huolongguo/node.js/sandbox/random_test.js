da26b1e07c1be9d72f7d37aee79c8285a5e3f13f
Vincent Viallet <balou@ip-192-168-0-116.ap-northeast-1.compute.internal>
2011-07-01 15:06:38 +0800

    Add sandbox / tests for node.js

---
+++ huolongguo/node.js/sandbox/random_test.js
@@ -0,0 +1,49 @@
+
+
+
+//var util = require("util"), exec = require('child_process').exec;
+//var mysql = require("./mysql.js")
+
+
+//var ls = exec('ls', function(error, stdout, stderr){
+//    console.log('ls' + stdout);
+//});
+
+//var grep = exec('grep test', function(error, stdout, stderr){
+//    console.log('grep' + stdout);
+//});
+
+//console.log('date.listeners : ' + util.inspect(date.listeners()));
+
+//ls.stdout.on('data', function (data) {
+//    grep.stdin.write(data);
+//});
+
+//ls.on('exit', function (code) {
+//    grep.stdin.end();
+//});
+
+//grep.stdout.on('data', function (data) {
+//    console.log('grep_bis : ' + data);
+//});
+
+//date.stdin.resume();
+
+//date.stdin.write('');
+//date.stdin.end();
+
+
+//var sleep = exec('sleep 2', function(error, stdout, stderr){
+//    console.log(stdout);
+//    console.log('error ' + error);
+//    if (error === null) {
+//        exec('date', function(error, stdout, stderr){
+//             console.log('deuxieme' + stdout);
+//             console.log('2bis ' + date.stdout);
+//       });
+         
+//    }
+//});
+
+
+//exports.start = start;
