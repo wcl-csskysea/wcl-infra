96d1ef42a55af38e72cc79e3337d9c2981c15b6a
Guinan Huang <guinan.huang@wiredcraft.com>
2022-01-07 18:02:52 +0800

    1. cluster_labels correction 2. ssh-tunnel script adjust 3. teleport-agent prompt information adjust 4. github.yaml adjust 5. user logins'user adjust

--- services/teleport/ansible/setup/templates/root-cluster/scripts/github-proxy-tunnel.sh
+++ services/teleport/ansible/setup/templates/root-cluster/scripts/github-proxy-tunnel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -u wcladmin -s /usr/bin/autossh -f -M 22000 -D 127.0.0.1:1112 -N -i /home/wcladmin/.ssh/weblate.pub wcladmin@103.61.38.70 -p 40022 >> /var/log/reverse_tunnel_github_proxy.log 2>&1
+/usr/bin/autossh -f -M 22000 -D 127.0.0.1:1112 -N -i /home/wcladmin/.ssh/weblate wcladmin@103.61.38.70 -p 40022 
